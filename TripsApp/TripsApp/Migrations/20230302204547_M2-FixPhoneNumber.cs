4ece3c37ced28f9d4d5da874875f055acd2620c9
claytonmayfield <clayton_mayfield@insideranken.org>
2023-04-24 12:55:39 -0500

    first commit

---
+++ TripsApp/TripsApp/Migrations/20230302204547_M2-FixPhoneNumber.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TripsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class M2FixPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trips",
+                keyColumn: "TripId",
+                keyValue: 2,
+                column: "AccommodationPhone",
+                value: "503-555-1234");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Trips",
+                keyColumn: "TripId",
+                keyValue: 2,
+                column: "AccommodationPhone",
+                value: "503-555-123");
+        }
+    }
+}
---
+++ TripsApp/TripsApp/Models/TripLogContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TripsApp.Models
+{
+    public class TripLogContext : DbContext
+    {
+      public TripLogContext(DbContextOptions<TripLogContext> options)
+            : base(options) { }
+
+      public DbSet<Trip> Trips { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Trip>().HasData(
+                new Trip
+                {
+                    TripId = 1,
+                  Destination = "Boise",
+                    StartDate = new DateTime(2023, 6, 6, 15, 0, 0, 0),
+                    EndDate = new DateTime(2023, 6, 14, 11, 0, 0, 0),
+                 Accommodation = "",
+                    AccommodationPhone = "",
+                  AccommodationEmail = "",
+                   ThingToDo1 = "Visit Tammy",
+                    ThingToDo2 = "",
+                    ThingToDo3 = ""
+                },
+                new Trip
+                {
+                    TripId = 2,
+                 Destination = "Portland",
+                    StartDate = new DateTime(2024, 1, 1, 15, 0, 0, 0),
+                 EndDate = new DateTime(2024, 1, 7, 11, 0, 0, 0),
+                    Accommodation = "The Benson Hotel",
+                  AccommodationPhone = "503-555-1234",
+                    AccommodationEmail = "staff@bensonhotel.com",
+                  ThingToDo1 = "Go to Voodoo Doughnuts",
+                    ThingToDo2 = "Walk in the rain",
+                  ThingToDo3 = "Go to Powell's"
+                }
+            );
+        }
+    }
+}
